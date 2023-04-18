19a8acb9225186c972ccdc1e8917fc77c350ddf8
許志磊 <accle7905231@gmail.com>
2023-04-19 01:15:56 +0800

    Building GUI, body status and show these value

--- CVdetection.py
+++ CVdetection.py
@@ -13,7 +13,7 @@ import cv2
 def topbottomConv (height, y):
     return height - y
 
-def make_request_handler(results):
+def make_request_handler(results, width, height):
     """ results: list to store results """
     if not isinstance(results, list):
         raise ValueError("results must be a list")
@@ -25,8 +25,35 @@ def make_request_handler(results):
             observations = request.results()
             for text_observation in observations:
                 recognized_text = text_observation.topCandidates_(1)[0]
-                results.append([recognized_text.string(), recognized_text.confidence()])
+                textrect = text_observation.boundingBox()
+                textrect.origin.x *= width
+                textrect.origin.y *= height
+                textrect.size.width *= width
+                textrect.size.height *= height
+                results.append([recognized_text.string(), recognized_text.confidence(), textrect])
     return handler
+
+
+class BodyStatus:
+
+    # Initializing
+    def __init__(self):
+        self.vision_handler = Vision.VNImageRequestHandler.alloc()
+        self.vision_request = Vision.VNRecognizeTextRequest.alloc()
+    def inputimage(self, cimage, recrect):
+        width, height = cimage.size
+        #origin blx = 74, bly = 92, trx = 218 try = 28 (base on 1200, 983)
+        body_image = cimage.crop(recrect) # have bug!!!
+        self.vision_handler.initWithCIImage_options_(body_image.ciimage, None)
+        results = []
+        handler = make_request_handler(results, width, height)
+        self.vision_request.initWithCompletionHandler_(handler)
+        error = self.vision_handler.performRequests_error_([self.vision_request], None)
+        return results
+    # Calling destructor
+    def __del__(self):
+        self.vision_handler.dealloc()
+        self.vision_request.dealloc()
 
 if __name__ == '__main__':
     count = 70
@@ -37,7 +64,6 @@ if __name__ == '__main__':
     while i < count:
         img = cimg.fromFile("/Users/xuzhilei/Desktop/flyff_pic/screenshot_" + str(i) +".jpg")
         width, height = img.size
-        print("x :", 206,"y :", topbottomConv(1292, 200))
         body_image = img.crop(206,  topbottomConv(1292, 200), 308, 136)
         target_image = img.crop(604, topbottomConv(1292, 132), 546, 138)
         vision_handler.initWithCIImage_options_(body_image.ciimage, None)
